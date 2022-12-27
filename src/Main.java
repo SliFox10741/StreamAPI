4272d2c87e4b7bf81aec87684199edb90070839e
Maxim <mr.maximgus@gmail.com>
2022-12-27 16:40:22 +0500

    Выполнен поиск кол-ва несовершеннолетних

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10/*_000_000*/; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        Stream<Person> stream = persons.stream();
+
+        //поиск кол-ва несовершеннолетних
+        long count = stream.filter(person -> person.getAge() < 18).count();
+        System.out.println(count);
+        
+    }
+}
